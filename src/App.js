981d1ee9def4a770363ca11267540a6a005dbe9f
Gabriel Galera <gabgalera@hotmail.com>
2022-10-03 19:02:01 -0300

    Requisito 2

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Route, Switch } from 'react-router-dom';
+import { Route, Switch, Redirect } from 'react-router-dom';
 
 import Login from './services/pages/Login';
 import Search from './services/pages/Search';
@@ -8,9 +8,41 @@ import Favorites from './services/pages/Favorites';
 import Profile from './services/pages/Profile';
 import ProfileEdit from './services/pages/ProfileEdit';
 import NotFound from './services/pages/NotFound';
+import Carregando from './services/pages/Carregando';
+
+import { createUser } from './services/userAPI';
 
 class App extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      logado: false,
+      muda: false,
+    };
+  }
+
+  componentDidUpdate() {
+    const { logado } = this.state;
+    console.log(logado);
+  }
+
+  getLogado = () => {
+    this.setState({ logado: true }, () => {
+      createUser({ name: 'Name' })
+        .then(() => this.setState(
+          { logado: false,
+            muda: true,
+          },
+          () => { (<Redirect to="/search" />); },
+        ));
+    });
+  };
+
   render() {
+    const { logado, muda } = this.state;
+
+    if (logado === true) return <Carregando />;
     return (
       <>
         <p>TrybeTunes</p>
@@ -20,7 +52,19 @@ class App extends React.Component {
           <Route path="/favorites" component={ Favorites } />
           <Route path="/album/:id" render={ () => <Album /> } />
           <Route path="/search" component={ Search } />
-          <Route exact path="/" component={ Login } />
+          <Route
+            exact
+            path="/"
+          >
+            {
+              muda === false
+                ? <Login
+                  logado={ logado }
+                  getLogado={ this.getLogado }
+                />
+                : <Redirect to="/search" />
+            }
+          </Route>
           <Route path="*" component={ NotFound } />
         </Switch>
       </>
--- src/services/pages/Login.js
+++ src/services/pages/Login.js
@@ -1,9 +1,62 @@
 import React from 'react';
+// import { Redirect } from 'react-router-dom';
+// import { createUser } from '../userAPI';
+// import Carregando from './Carregando';
+// import Search from './Search';
 
 class Login extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      length: 0,
+      disabled: true,
+    };
+  }
+
+  componentDidUpdate() {
+
+  }
+
+  handleChange = (e) => {
+    this.setState(() => ({
+      length: e.target.value.length,
+    }), () => {
+      const { length } = this.state;
+      const minLength = 3;
+      if (length >= minLength) {
+        this.setState({
+          disabled: false,
+        });
+      }
+    });
+  };
+
   render() {
+    const { disabled } = this.state;
+    const { getLogado } = this.props;
+    // const createUser = this.props;
+    // console.log(getLogado);
     return (
-      <div data-testid="page-login" />
+      <div data-testid="page-login">
+        <input
+          type="text"
+          data-testid="login-name-input"
+          onChange={ (e) => this.handleChange(e) }
+        />
+        <button
+          className="btn-form"
+          data-testid="login-submit-button"
+          type="button"
+          disabled={ disabled }
+          onClick={ () => {
+            getLogado();
+          } }
+        >
+          Entrar
+        </button>
+
+      </div>
     );
   }
 }
