d6c678754fab0366d437a97cdcd98ff72399713c
Gabriel Galera <gabgalera@hotmail.com>
2022-10-04 17:58:47 -0300

    Requisito 8

--- src/components/MusicCard.js
+++ src/components/MusicCard.js
@@ -1,11 +1,58 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
+import { addSong } from '../services/favoriteSongsAPI';
+import Carregando from '../services/pages/Carregando';
+
 class MusicCard extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      isLoading: false,
+      favorites: [],
+    };
+  }
+
+  // shouldComponentUpdate(e) {
+  //   this.waitMusic(e);
+  // }
+
+  waitMusic = (e) => {
+    const { favorites } = this.state;
+    favorites.push(Number(e.target.id));
+    this.setState({
+      isLoading: true,
+      favorites,
+    }, () => {
+      console.log(favorites);
+      addSong().then(() => {
+        this.setState({
+          isLoading: false,
+        });
+      });
+    });
+  };
+
+  // check = (entry) => {
+  //   const { favorites } = this.state;
+  //   if (typeof entry.target !== 'undefined') {
+  //     const favorite = favorites.filter((fav) => fav === entry.target.id);
+  //     console.log(favorite);
+  //     return true;
+  //   }
+  //   const favorite = [];
+  //   console.log(entry);
+  //   return (favorite.length > 0);
+  // };
+
   render() {
+    const { isLoading, favorites } = this.state;
     let { musics } = this.props;
     musics = musics
       .filter((music) => typeof music.trackId !== 'undefined');
+
+    if (isLoading === true) return <Carregando />;
 
     return (
       <div>
@@ -24,6 +71,16 @@ class MusicCard extends React.Component {
                 {' '}
                 <code>audio</code>
               </audio>
+              <label htmlFor={ music.trackId }>
+                Favorita
+                <input
+                  type="checkbox"
+                  data-testid={ `checkbox-music-${music.trackId}` }
+                  id={ music.trackId }
+                  checked={ favorites.includes(music.trackId) }
+                  onChange={ (e) => this.waitMusic(e) }
+                />
+              </label>
             </span>
 
           )) }
@@ -33,7 +90,7 @@ class MusicCard extends React.Component {
 }
 
 MusicCard.propTypes = {
-  musics: PropTypes.shape().isRequired,
+  musics: PropTypes.arrayOf(PropTypes.shape()).isRequired,
 };
 
 export default MusicCard;
