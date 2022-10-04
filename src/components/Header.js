c0ad6337db89f193e7f5ae637c12c2fb95506ba3
Gabriel Galera <gabgalera@hotmail.com>
2022-10-04 09:28:10 -0300

    Requisito 4

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import Carregando from '../services/pages/Carregando';
 import { getUser } from '../services/userAPI';
 
@@ -41,6 +42,11 @@ class Header extends React.Component {
           {response}
           !
         </span>
+        <nav>
+          <Link to="/search" data-testid="link-to-search">Search</Link>
+          <Link to="/favorites" data-testid="link-to-favorites">Favorites</Link>
+          <Link to="/profile" data-testid="link-to-profile">Profile</Link>
+        </nav>
       </header>
     );
   }
